ec0c997e7d134876e953743b773155a5ce42ac4c
JGMelon22 <jgvasconcellosm22@outlook.com>
2024-06-20 13:50:35 -0300

    tests: improving tests for Brand Repository

--- Weindrachen.Tests/Repository/BrandRepositoryTests.cs
+++ Weindrachen.Tests/Repository/BrandRepositoryTests.cs
@@ -1,6 +1,8 @@
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
+using Namotion.Reflection;
 using Weindrachen.DTOs.Brand;
+using Weindrachen.DTOs.Grape;
 using Weindrachen.Infrastructure.Data;
 using Weindrachen.Infrastructure.Repositories;
 using Weindrachen.Models;
@@ -38,54 +40,93 @@ public class BrandRepositoryTests
     }
 
     [Fact]
-    public void BrandRepository_AddBrand_ReturnsBrand()
+    public async Task BrandRepository_AddBrand_ReturnsBrand()
     {
-        var brandInput = new BrandInput("Concha y Toro", Country.Chile);
-        var result = _repository.AddNewBrandAsync(brandInput);
-
+        // Arrange
+        var newBrand = new BrandInput("Concha y Toro", Country.Chile);
+        var brandResult = new BrandResult
+        {
+            Id = 11,
+            Name = "Concha y Toro",
+            Country = Country.Chile
+        };
+        
+        // Act
+        var result = await _repository.AddNewBrandAsync(newBrand);
+        
+        // Assert
         result.Should().NotBeNull();
+        result.Data.Should().BeEquivalentTo(brandResult);
+        result.Should().BeOfType<ServiceResponse<BrandResult>>();
     }
 
     [Fact]
-    public void BrandRepository_GetAllBrands_ReturnsBrands()
+    public async Task BrandRepository_GetAllBrands_ReturnsBrands()
     {
-        var result = _repository.GetAllBrandsAsync();
+        // Act
+        var result = await _repository.GetAllBrandsAsync();
 
+        // Assert
         result.Should().NotBeNull();
-        result.Should().BeOfType<Task<ServiceResponse<IEnumerable<BrandResult>>>>();
+        result.Data!.Count().Should().Be(10);
+        result.Should().BeOfType<ServiceResponse<IEnumerable<BrandResult>>>();
     }
 
     [Fact]
-    public void BrandRepository_GetBrandById_ReturnsBrand()
+    public async Task BrandRepository_GetBrandById_ReturnsBrand()
     {
-        var id = 1;
-
-        var result = _repository.GetBrandByIdAsync(id);
-
+        // Arrange
+        int id = 1;
+        var brandResult = new BrandResult
+        {
+            Id = 1,
+            Name = "New Fake Brand",
+            Country = Country.Argentina
+        };
+        
+        // Act
+        var result = await _repository.GetBrandByIdAsync(id);
+        
+        // Assert
         result.Should().NotBeNull();
-        result.Should().BeOfType<Task<ServiceResponse<BrandResult>>>();
+        result.Data.Should().BeEquivalentTo(brandResult);
+        result.Should().BeOfType<ServiceResponse<BrandResult>>();
     }
 
     [Fact]
-    public void BrandRepository_UpdateBrand_ReturnsBrands()
+    public async Task BrandRepository_UpdateBrand_ReturnsBrands()
     {
-        var id = 4;
+        // Arrange
+        int id = 4;
         var updatedBrand = new BrandInput("Catena Zapata", Country.Argentina);
+        var brandResult = new BrandResult
+        {
+            Id = 4,
+            Name = "Catena Zapata",
+            Country = Country.Argentina
+        };
 
-        var result = _repository.UpdateBrandAsync(id, updatedBrand);
+        // Act
+        var result = await _repository.UpdateBrandAsync(id, updatedBrand);
 
+        // Assert
         result.Should().NotBeNull();
-        result.Should().BeOfType<Task<ServiceResponse<BrandResult>>>();
+        result.Data.Should().BeEquivalentTo(brandResult);
+        result.Should().BeOfType<ServiceResponse<BrandResult>>();
     }
 
     [Fact]
-    public void BrandRepository_RemoveBrand_ReturnsSuccess()
+    public async Task BrandRepository_RemoveBrand_ReturnsSuccess()
     {
-        var id = 3;
+        // Arrange
+        int id = 3;
 
-        var result = _repository.RemoveBrandAsync(id);
+        // Act 
+        var result = await _repository.RemoveBrandAsync(id);
 
+        // Assert
         result.Should().NotBeNull();
-        result.Should().BeOfType<Task<ServiceResponse<bool>>>();
+        result.Success.Should().Be(true);
+        result.Should().BeOfType<ServiceResponse<bool>>();
     }
 }
