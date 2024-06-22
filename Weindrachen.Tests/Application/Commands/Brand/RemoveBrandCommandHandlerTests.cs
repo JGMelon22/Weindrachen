748c0ec40e78654e82b309d15aa172371e2597d6
JGMelon22 <jgvasconcellosm22@outlook.com>
2024-06-22 14:07:35 -0300

    tests: implementing Remove Command Handler test for Brands

---
+++ Weindrachen.Tests/Application/Commands/Brand/RemoveBrandCommandHandlerTests.cs
@@ -0,0 +1,43 @@
+using FakeItEasy;
+using FluentAssertions;
+using Weindrachen.Application.Commands.Brand;
+using Weindrachen.Application.Handlers.Brand;
+using Weindrachen.Infrastructure.Repositories;
+using Weindrachen.Interfaces;
+using Weindrachen.Models;
+
+namespace Weindrachen.Tests.Application.Commands.Brand;
+
+public class RemoveBrandCommandHandlerTests
+{
+    private readonly IBrandRepository _brandRepository;
+
+    public RemoveBrandCommandHandlerTests()
+    {
+        _brandRepository = A.Fake<IBrandRepository>();
+    }
+
+    [Fact]
+    public async Task RemoveBrandCommandHandler_Handle_ReturnsSuccess()
+    {
+        // Arrange
+        int id = 1;
+        var handler = new RemoveBrandHandler(_brandRepository);
+
+        var serviceResponse = new ServiceResponse<bool>();
+
+        A.CallTo(() => _brandRepository.RemoveBrandAsync(id))
+            .Returns(Task.FromResult(serviceResponse));
+
+        var command = new RemoveBrandCommand(id);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Success.Should().BeTrue();
+        A.CallTo(() => _brandRepository.RemoveBrandAsync(id))
+            .MustHaveHappenedOnceExactly();
+    }
+}
